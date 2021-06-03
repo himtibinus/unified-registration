9b23190c70dad1457071fcf7681a1c58369bf8c7
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-06-03 12:53:24 +0700

    feat: Update Composer

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'sebastian/type' => '2.3.1@81cd61ab7bbf2de744aba0ea61fae32f721df3d2',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'laravel/laravel' => 'dev-main@0f98dcb66c7910861f81d13d0bc19eafeda2e39f',
+  'laravel/laravel' => 'dev-main@589c941cad094e38a8645396f2efd750e4af4d93',
 );
 
     private function __construct()
