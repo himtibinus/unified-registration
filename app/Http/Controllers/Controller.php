f4f6a7f1ae5b30c5f504703c7fe39f551b3ff617
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-07-05 20:50:29 +0700

    chore: Promote requiresLogin and validateFields to main controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -50,4 +50,69 @@ class Controller extends BaseController
             'committee' => $committee
         ];
     }
+
+    /**
+     * Utility function to ensure that the user has been authenticated
+     */
+    private function requiresLogin(string $path, int $eventId, bool $for_admin, bool $for_committee){
+        if (!Auth::check()){
+            Session::put('error', 'Please log in before continuing to this page');
+            Session::put('loginTo', $path);
+            return false;
+        }
+
+        $check = $this->checkAdminOrCommittee(Auth::id(), $eventId);
+
+        if (($check->admin && $for_admin) || ($check->committee && $for_committee) == true) return true;
+        Session::put('error', 'You are not authorized to access this page');
+        return false;
+    }
+
+    public function validateFields($event_permissions, $user_properties)
+    {
+        $eligible_to_register = true;
+        for ($i = 0, $j = 0; $i < count($event_permissions) && $j < count($user_properties); $i++, $j++){
+            $diff = strcmp($event_permissions[$i]->field_id, $user_properties[$j]->field_id);
+            // If equal
+            if ($diff == 0){
+                $event_permissions[$i]->current_value = $user_properties[$j]->value;
+                // Validate with regex
+                if (strlen($event_permissions[$i]->validation_rule) == 0 || preg_match($event_permissions[$i]->validation_rule, $user_properties[$j]->value)){
+                    $event_permissions[$i]->satisfied = true;
+                } else {
+                    $eligible_to_register = false;
+                }
+            }
+            // If $user_properties[$j]'s ASCII difference is less than $event_permissions[$i]
+            else if ($diff > 0){
+                // Try to repeat the loop
+                // Increment $j but not $i
+                $i--;
+            }
+            // Else if the field is not found
+            else {
+                // Is the field required or optional?
+                if ($event_permissions[$i]->required){
+                    $eligible_to_register = false;
+                }
+                // Increment $i but not $j
+                $j--;
+            }
+        }
+        // If the last required permissions is not satisfied
+        if ($i > 0 && !isset($event_permissions[$i - 1]->satisfied) && $event_permissions[$i - 1]->required){
+            $eligible_to_register = false;
+        }
+        // Check whether there are validations which have not been checked
+        if ($i < count($event_permissions)) for (; $i < count($event_permissions); $i++){
+            if ($event_permissions[$i]->required){
+                $eligible_to_register = false;
+            }
+        }
+        // Return a new stdClass
+        return (object) [
+            'eligible_to_register' => $eligible_to_register,
+            'event_permissions' => $event_permissions
+        ];
+    }
 }
