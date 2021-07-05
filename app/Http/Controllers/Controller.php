b0be030f6a44d2ee188568b9a2e68e85f4bfbc6f
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-07-05 20:48:00 +0700

    chore: Promote checkAdminOrCommittee to main controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,44 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    private function checkAdminOrCommittee($userId, $eventId){
+        $admin = false;
+        $committee = false;
+
+        // Check whether the user is an admin or comittee
+        $select = DB::table('user_properties')->where('user_id', $userId);
+        $query = $select->where('field_id', 'role.administrator')->first();
+        if ($query && $query->value == '1'){
+            $admin = true;
+            $committee = true;
+        }
+        $select = DB::table('user_properties')->where('user_id', $userId);
+        $query = $select->where('field_id', 'role.committee')->first();
+        if ($query && $query->value == '1'){
+            $committee = true;
+        }
+        if (!$admin && !$committee && $eventId > 0){
+            // Check whether Event ID exists
+            $select = DB::table('events')->where('id', $eventId)->first();
+            if ($select){
+                $select = DB::table('event_roles')->where('event_id', $eventId);
+                $query = $select->where('user_id', $userId)->where('system_role', 'role.administrator')->first();
+                if ($query){
+                    $admin = true;
+                    $committee = true;
+                }
+                $select = DB::table('event_roles')->where('event_id', $eventId);
+                $query = $select->where('user_id', $userId)->where('system_role', 'role.committee')->first();
+                if ($query){
+                    $committee = true;
+                }
+            }
+        }
+
+        return (object) [
+            'admin' => $admin,
+            'committee' => $committee
+        ];
+    }
 }
