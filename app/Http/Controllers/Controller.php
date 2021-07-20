2910c9e0ecbde7df1767d469b1ba0beff90bb10a
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-07-20 16:04:04 +0700

    feat: Require Session on Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class Controller extends BaseController
 {
