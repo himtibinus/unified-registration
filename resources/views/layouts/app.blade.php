c90deeddb1072d1f78d05e1780e6e9bf0c7b50e7
NatanaelGeraldoS <geradonatanael84@gmail.com>
2022-05-28 19:02:18 +0700

    ADD: Favicon Logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <script type="module" src="/pwabuilder-sw-register.js"></script>
     <meta charset="utf-8">
@@ -9,12 +10,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <link rel="shortcut icon" href="{{ url('./assets/himti-mono.png') }}" type="image/x-icon">
     <!-- Scripts -->
     <script src="{{ url('/js/app.js') }}"></script>
     <script src="{{ url('/js/cssua.min.js') }}"></script>
     <script>
-        function adjustDate(element){
+        function adjustDate(element) {
             // Localize date and time
             var eventDate = document.getElementById(element);
             var date = new Date(Date.parse(eventDate.textContent + "Z"));
@@ -29,6 +30,7 @@
     <!-- Styles -->
     <link href="{{ url('/css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark shadow-sm fixed-top w-100">
@@ -42,7 +44,9 @@
                 </a>
                 <p class="app-mobile-title d-md-none app-title mb-0 fw-bold text-white">
                     HIMTI Registration</p>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -51,24 +55,27 @@
                         @guest
                             @if (Route::has('register'))
                                 <li class="nav-item me-4">
-                                    <a class="nav-link fw-bold" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link fw-bold"
+                                        href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                             @if (Route::has('login'))
-                            <li class="nav-item nav-item-custom d-flex">
-                                <a class="btn btn-light d-flex align-items-center gap-1 lh-normal" href="{{ route('login') }}">                                                             <i class="bi bi-box-arrow-in-left"></i>
-                                    <span>Login</span>
-                                </a>
-    
-                                <form id="logout-form" action="https://testing.himti.or.id/logout" method="POST" class="d-none">
-                                    <input type="hidden" name="_token" value="p8QEcoj1U8jpS7kLeX8R87Y12LYjPQLIXChCWtgQ">                            </form>
-                            </li>
-                            @endif
+                                <li class="nav-item nav-item-custom d-flex">
+                                    <a class="btn btn-light d-flex align-items-center gap-1 lh-normal"
+                                        href="{{ route('login') }}"> <i class="bi bi-box-arrow-in-left"></i>
+                                        <span>Login</span>
+                                    </a>
 
-                            
+                                    <form id="logout-form" action="https://testing.himti.or.id/logout" method="POST"
+                                        class="d-none">
+                                        <input type="hidden" name="_token" value="p8QEcoj1U8jpS7kLeX8R87Y12LYjPQLIXChCWtgQ">
+                                    </form>
+                                </li>
+                            @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle " href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     <i class="bi bi-person-circle me-1"></i>
                                     <span>{{ Auth::user()->name }}</span>
                                 </a>
@@ -77,14 +84,14 @@
                                     <a class="dropdown-item" href="/profile">
                                         <i class="bi bi-person-circle"></i> Profile
                                     </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
                                         <i class="bi bi-box-arrow-right"></i>
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
                                         @csrf
                                     </form>
                                 </div>
@@ -100,8 +107,10 @@
         </main>
         <footer class="container">
             <hr>
-            <p class="text-center">© 2021–2022 Research and Development, <a href="https://himti.or.id" class="backlink-himti text-decoration-none">HIMTI BINUS University</a>.</p>
+            <p class="text-center">© 2021–2022 Research and Development, <a href="https://himti.or.id"
+                    class="backlink-himti text-decoration-none">HIMTI BINUS University</a>.</p>
         </footer>
     </div>
 </body>
+
 </html>
